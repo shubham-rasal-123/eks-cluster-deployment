226a451fc5204015bc746617a3ea124761643da3
shubham-rasal-123 <shubhamrasal861@gmail.com>
2026-08-11 15:32:54 +0530

    added main tf file

---
+++ main.tf
@@ -0,0 +1,53 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.example.arn
+  version  = "1.35"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = data.aws_subnets.default.ids
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  instance_types = ["t3.small"]
+  disk_size      = "20"
+  capacity_type  = "ON_DEMAND"
+  labels         = tomap({ env = "dev" })
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
